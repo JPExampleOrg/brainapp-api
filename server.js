5a962d23b593dbc20aac43392f1446a5b5bcebc2
Juan Pablo Castano <jpcastanom@gmail.com>
2018-06-19 13:51:25 -0400

    changed database to heroku postgresql

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'juanpablocastano',
-    password : '',
-    database : 'facerecognitionbrain-db'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
